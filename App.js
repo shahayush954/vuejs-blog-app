7c40f829b3d4cac98e0be4ca52a87a4c7a4fe927
ayush.sha <ayush.sha@internal.media.net>
2021-03-27 16:02:44 +0530

    Events

--- App.js
+++ App.js
@@ -5,10 +5,26 @@ new Vue({
         job: "Software Developer",
         website: "https://shahayush954.herokuapp.com",
         websiteHTML: "<a href='https://shahayush954.herokuapp.com'>Visit Website</a>",
+        age: 25,
+        x: 0,
+        y: 0,
     },
     methods: {
         greet: function(time) {
             return "Good " + time + " " + this.name;
+        },
+
+        add: function(inc){
+            this.age += inc;
+        },
+
+        subtract: function(dec){
+            this.age -= dec;
+        },
+
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
