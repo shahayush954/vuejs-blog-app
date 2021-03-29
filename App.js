41c4a0be7dbd5622ca279adac5086e5f1c16b171
ayush.sha <ayush.sha@internal.media.net>
2021-03-29 15:35:28 +0530

    completed game

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 new Vue({
-    el: "#root",
+    el: "#vue-app",
     data: {
-        name: "Ayush"
+        health: 100,
+        ended: false,
+    },
+
+    methods: {
+        punch: function(){
+            this.health -= 10;
+            if(this.health <= 0){
+                this.ended = true;
+            }
+        },
+
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
     }
 });
